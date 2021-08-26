b972963365059832747883ad5579bc3edbe84af9
Karpierz, Adam <adam.karpierz@aptiv.com>
2021-08-26 19:41:56 +0200

    1.2.3b7 (2021-08-26) -------------------- - Fixes for Linux. - Fixes for setup.

--- src/pcap/__about__.py
+++ src/pcap/__about__.py
@@ -3,4 +3,4 @@
 # https://opensource.org/licenses/BSD-3-Clause
 
 __import__("pkg_about").about()
-__copyright__ = f"Copyright (c) 2016-2021, {__author__}"
+__copyright__ = f"Copyright (c) 2016-2021, {__author__}"  # noqa
